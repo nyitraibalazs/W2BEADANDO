6e6fcd157fff5fb4a69bce54a0acef5360b04b88
nyitraibalazs <nyitraibalazs.h@gmail.com>
2019-05-26 11:55:29 +0200

    V3

--- src/tests/Test2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MainPageCustomer } from '../components/mainPageCustomer';
-
-it('MainPageCustomer renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainPageCustomer customerData={{name: "", email: "", address: ""}} ownOrders={[]} />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/tests/Test5.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { IndexCustomer } from '../components/indexCustomer';
-
-it('IndexCustomer renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<IndexCustomer />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
